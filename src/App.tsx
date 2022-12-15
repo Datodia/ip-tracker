7a283b394d7486dc9eda0e17836c2260d8bc146a
Datodia <dato.diasamidze.02@gmail.com>
2022-12-15 16:12:34 +0400

    feat: resolve location bug and useeffect bug

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,15 @@ function App() {
 
   const [IP, setIP] = useState<string>("")
   const [data, setData] = useState<Data>()
-  const [lat, setLat] = useState<number>(0)
+  const [lat, setLat] = useState<number>(41.125)
   const [lng, setLng] = useState<number>(0)
 
   const svgIcon = L.divIcon({
     html: `
     <svg xmlns="http://www.w3.org/2000/svg" 
     width="46" 
-    height="56">
+    height="56"
+    border="3px solid red">
     <path fill-rule="evenodd" 
     d="M39.263 7.673c8.897 8.812 8.966 23.168.153 32.065l-.153.153L23 56 6.737 39.89C-2.16 31.079-2.23 16.723 6.584 7.826l.153-.152c9.007-8.922 23.52-8.922 32.526 0zM23 14.435c-5.211 0-9.436 4.185-9.436 9.347S17.79 33.128 23 33.128s9.436-4.184 9.436-9.346S28.21 14.435 23 14.435z"/>
     </svg>`,
@@ -29,6 +30,8 @@ function App() {
   L.Marker.prototype.options.icon = svgIcon;
 
 
+  const city = data?.location.region.slice(-8).replace(`'`, "")
+
   const search = async () => {
     const respons = await axios.get(`https://geo.ipify.org/api/v2/country,city?apiKey=at_Np7ew5wGSQdfdRxr1fCsqnlbWFwh5&ipAddress=${IP}`)
     const Data: Data = await respons.data
@@ -37,11 +40,19 @@ function App() {
 
     setLat(Data.location.lat)
     setLng(Data.location.lng)
+
+    // if (Data) {
+    //   setLat(Data.location.lat)
+    //   setLng(Data.location.lng)
+    // } else {
+    //   setLat(41.225)
+    //   setLng(42.225)
+    // }
   }
 
-  useEffect(() => {
-    search()
-  }, [])
+  // useEffect(() => {
+  //   search()
+  // }, [])
 
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -77,19 +88,19 @@ function App() {
           <Result>
             <Div>
               <ResultTitle>IP ADRESS</ResultTitle>
-              <ResultDesc>{data?.ip}</ResultDesc>
+              <ResultDesc>{data ? data.ip : `8.8.8.8`}</ResultDesc>
             </Div>
             <Div>
               <ResultTitle>LOCATION</ResultTitle>
-              <ResultDesc>{`${data?.location.city}, ${data?.location.country} ${data?.location.postalCode}`}</ResultDesc>
+              <ResultDesc>{data ? data.location.city === "" ? `${city}, ${data.location.country} ${data.location.postalCode}` : `${data.location.city}, ${data.location.country} ${data.location.postalCode}` : `Tbilisi, GE`}</ResultDesc>
             </Div>
             <Div>
               <ResultTitle>TIME ZONE</ResultTitle>
-              <ResultDesc>{data?.location.timezone}</ResultDesc>
+              <ResultDesc>{data ? data.location.timezone : '+04:00'}</ResultDesc>
             </Div>
             <Div>
               <ResultTitle>ISP</ResultTitle>
-              <ResultDesc>{data?.isp}</ResultDesc>
+              <ResultDesc>{data ? data.isp : `Google LLC`}</ResultDesc>
             </Div>
           </Result>
         </Wrapper>
@@ -102,9 +113,6 @@ function App() {
         />
         <LiveLocation center={[lat, lng]} />
         <Marker position={[lat, lng]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
         </Marker>
       </MapContainer>
     </Container>
